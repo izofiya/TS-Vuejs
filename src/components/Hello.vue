6ef8eb053654763834a0b980b6c72ab95faf9c50
izofiya <izofiya@mail.ru>
2019-10-06 23:09:40 +0300

    add prop

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,16 +3,22 @@
     <h1>{{ message }}</h1>
     <button @click="clicked">Click</button>
     <button @click="parentClicked">Parent click</button>
+    <MyComp msg="It is MyComp"></MyComp>
   </div>
 </template>
 
 <script lang="ts">
 import Parent from './Parent';
-import Component from 'vue-class-component';
+// import Component from 'vue-class-component';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import MyComp from './MyComp.vue';
 
-@Component({})
+@Component({
+  components: {
+    MyComp
+  }
+})
 export default class Hello extends Parent {
-  // message: string = 'Hello Vue'
 
   created() {
     console.log('child created!');
---
+++ src/components/MyComp.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3 class="message">{{msg}}</h3>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class MyComp extends Vue {
+    @Prop() private msg: string;
+}
+</script>
